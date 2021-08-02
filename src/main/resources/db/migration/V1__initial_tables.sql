09efb4a59d62c2d11b680fb82b5b7a4e077ed1ef
fusion525 <fusion525@users.noreply.github.com>
2021-08-02 22:53:02 +0200

    SQL script fixed

--- src/main/resources/db/migration/V1__initial_tables.sql
+++ src/main/resources/db/migration/V1__initial_tables.sql
@@ -1,23 +1,20 @@
-CREATE TABLE courts (
-    court_id bigint AUTO_INCREMENT,
-    court_open date,
-    court_close date
-
-    primary key (court_id)
-    );
-
-CREATE TABLE customers (
-    cust_id bigint AUTO_INCREMENT,
-    cust_name varchar(200),
-    cust_email varchar(200),
-    cust_phone varchar(100)
-
-    primary key (cust_id)
+CREATE TABLE `courts` (
+    `court_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `court_open` TIME,
+    `court_close` TIME,
+    PRIMARY KEY (`court_id`)
 );
 
-CREATE TABLE reservations (
-    res_id bigint AUTO_INCREMENT,
-    res_time date
+CREATE TABLE `customers` (
+    `cust_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `cust_name` VARCHAR(200),
+    `cust_email` VARCHAR(200),
+    `cust_phone` VARCHAR(100),
+     PRIMARY KEY (`cust_id`)
+);
 
-    primary key (res_id)
+CREATE TABLE `reservations` (
+    `res_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `res_time` DATE,
+    PRIMARY KEY (`res_id`)
 );
